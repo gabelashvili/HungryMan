535db5a0c8affe935d22a0b38af7eb81ef9a055b
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-05-10 22:27:34 +0400

    add auth btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
 import { useSelector } from '../../hooks/useSelector';
 import BasketIcon from '../../Icons/BasketIcon';
@@ -12,6 +12,8 @@ import SearchBar from './SearchBar/SearchBar';
 import UserMenu from './UserMenu/UserMenu';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const isUserAuthed = useSelector((state) => state.userReducer.user?.id);
   const selectedProductsInCart = useSelector((state) => state.productsReducer.selectedProductsCart);
   const [showMenu, setShowMenu] = useState<boolean>(false);
   const [showSearchBar, setShowSearchBar] = useState<boolean>(false);
@@ -49,19 +51,21 @@ const Header = () => {
             </span>
             )}
           </Button>
-          <div className="relative">
-            <button id="show-user-menu" className="button button--icon user--icon" onClick={() => setShowMenu(!showMenu)}>
-              <img
-                src={Logo}
-                alt="icon"
+          {isUserAuthed ? (
+            <div className="relative">
+              <button id="show-user-menu" className="button button--icon user--icon" onClick={() => setShowMenu(!showMenu)}>
+                <img
+                  src={Logo}
+                  alt="icon"
+                />
+              </button>
+              <UserMenu
+                handleClickOutside={() => setShowMenu(false)}
+                open={showMenu}
+                showMyAddressModal={() => setShowAddresses(true)}
               />
-            </button>
-            <UserMenu
-              handleClickOutside={() => setShowMenu(false)}
-              open={showMenu}
-              showMyAddressModal={() => setShowAddresses(true)}
-            />
-          </div>
+            </div>
+          ) : <Button handleClick={() => navigate('/auth')} type="secondary">ავტორიზაცია</Button>}
         </div>
       </header>
     </>
