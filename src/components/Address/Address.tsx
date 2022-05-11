30a90bfc57b35ff53f3a5acd4553053d0e221f5b
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-05-11 13:24:18 +0400

    fx

--- src/components/Address/Address.tsx
+++ src/components/Address/Address.tsx
@@ -1,18 +1,28 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useSelector } from '../../hooks/useSelector';
 import { toggleModal } from '../../store/ducks/modalsDuck';
+import { getUserAddresses } from '../../store/ducks/userDuck';
 import AddAddress from './AddAddress';
 import MyAddresses from './MyAddresses';
 
 const Address = () => {
   const dispatch = useAppDispatch();
+  const authedUserId = useSelector((state) => state.userReducer.user?.id);
   const { addAddress, myAddressList } = useSelector((state) => state.modalsReducer);
+  const addresses = useSelector((state) => state.userReducer.addresses);
 
   useEffect(() => {
     if (addAddress && myAddressList) {
       dispatch(toggleModal('myAddressList'));
     }
   }, [addAddress, myAddressList]);
+
+  useEffect(() => {
+    if (!addresses && authedUserId) {
+      dispatch(getUserAddresses(authedUserId));
+    }
+  }, [addresses, authedUserId]);
+
   return (
     <>
       {addAddress && <AddAddress />}
