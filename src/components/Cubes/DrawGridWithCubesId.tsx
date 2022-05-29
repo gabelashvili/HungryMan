eb22ee3590041527f21ddc7353601a733681d70e
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-05-29 17:09:28 +0400

    change color

--- src/components/Cubes/DrawGridWithCubesId.tsx
+++ src/components/Cubes/DrawGridWithCubesId.tsx
@@ -126,7 +126,7 @@ const drawRect = (
     x={x}
     y={y}
     key={id}
-    style={{ fill: !isSelected ? 'red' : color }}
+    style={{ fill: !isSelected ? 'transparent' : color }}
     id={id.toString()}
     onClick={(e) => handleClick(e)}
   />
