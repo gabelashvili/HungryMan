2702ab413504d901cf4d2ad82d4d25b9136f7482
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-06-07 15:42:40 +0400

    add select

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -1,20 +1,32 @@
 import ReactSelect, { StylesConfig } from 'react-select';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'chocolate', label: 'Chocolateqwdqwdqwdqqwdqwdd' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
 
 const Select = () => {
   return (
-    <ReactSelect menuIsOpen styles={SelectStyles} options={options} />
+    <ReactSelect styles={SelectStyles} options={options} />
   );
 };
 
 export default Select;
 
 const SelectStyles: StylesConfig = {
-  container: () => ({ width: '100%' }),
-  menuList: () => ({ width: 'max-content' }),
+//   menu: (styles) => ({ ...styles, width: '100%' }),
+  container: (styles) => ({ ...styles, width: '100%', height: '100%' }),
+  control: (styles) => ({
+    ...styles, width: '100%', height: '100%', background: '#112231', boxShadow: 'none', border: 'none',
+  }),
+  menuList: (styles) => ({ ...styles, background: '#112231' }),
+  option: (styles, state) => {
+    let bgColor = '#112231';
+    if (state.isSelected || state.isFocused) {
+      bgColor = '#050b10';
+    }
+    return { ...styles, background: bgColor };
+  },
+  singleValue: (styles) => ({ ...styles, color: 'white' }),
 };
