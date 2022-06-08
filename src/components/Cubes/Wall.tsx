2afef184a8bc54915d17e106ec2dae61e9e286a8
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-06-08 15:30:01 +0400

    add pan and zoom

--- src/components/Cubes/Wall.tsx
+++ src/components/Cubes/Wall.tsx
@@ -1,63 +1,14 @@
 import {
-  MouseEvent,
-  useEffect, useRef, useState, WheelEvent,
+  useEffect, useRef, useState,
 } from 'react';
+import { TransformWrapper, TransformComponent } from '@kokarn/react-zoom-pan-pinch';
 import {
   CUBES_TOTAL_COLUMNS, CUBES_TOTAL_ROWS, CUBE_DARK_COLOR, CUBE_LIGHT_COLOR,
 } from '../../Routes/Cubes/Cubes';
 
 const Wall = () => {
-  const [canvasOptions, setCanvasOptions] = useState({
-    panX: 0,
-    panY: 0,
-    scaleFactor: 1.00,
-  });
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const mouseStartPos = useRef({ x: 0, y: 0 });
-
-  const zoomIn = () => {
-    setCanvasOptions({ ...canvasOptions, scaleFactor: canvasOptions.scaleFactor * 1.1 });
-  };
-
-  const zoomOut = () => {
-    setCanvasOptions({ ...canvasOptions, scaleFactor: canvasOptions.scaleFactor / 1.1 });
-  };
-
-  const panUp = () => {
-    setCanvasOptions({ ...canvasOptions, panY: canvasOptions.panY - 25 });
-  };
-
-  const panDown = () => {
-    setCanvasOptions({ ...canvasOptions, panY: canvasOptions.panY + 25 });
-  };
-
-  const panLeft = () => {
-    setCanvasOptions({ ...canvasOptions, panX: canvasOptions.panX - 25 });
-  };
-
-  const panRight = () => {
-    setCanvasOptions({ ...canvasOptions, panX: canvasOptions.panX + 25 });
-  };
-
-  const onMouseWheel = (event:WheelEvent<HTMLCanvasElement>) => {
-    event.deltaY < 0 ? zoomIn() : zoomOut();
-  };
-
-  const handleMouseDown = (e: MouseEvent) => {
-    const startX = e.clientX;
-    const startY = e.clientY;
-    mouseStartPos.current = {
-      x: startX,
-      y: startY,
-    };
-  };
-
-  const handleMouseMove = (e:MouseEvent) => {
-    const { x, y } = mouseStartPos.current;
-    setCanvasOptions({ ...canvasOptions, panX: canvasOptions.panX - (x - e.clientX) });
-    console.log(x - e.clientX);
-  };
 
   // set context
   useEffect(() => {
@@ -79,27 +30,19 @@ const Wall = () => {
         canvasRef.current.width,
         canvasRef.current.height,
         cubeSize,
-        canvasOptions.scaleFactor,
-        canvasOptions.panX,
-        canvasOptions.panY,
       );
     }
-  }, [ctx, canvasOptions]);
+  }, [ctx]);
   return (
     <div style={{ width: '100%' }}>
-      <button onClick={zoomIn}>ZOOM IN</button>
-      <button onClick={zoomOut}>ZOOM OUT</button>
-      <button onClick={panUp}>PAN UP</button>
-      <button onClick={panDown}>PAN DOWN</button>
-      <button onClick={panLeft}>PAN LEFT</button>
-      <button onClick={panRight}>PAN RIGHT</button>
-      <canvas
-        ref={canvasRef}
-        style={{ width: '100%' }}
-        onWheel={onMouseWheel}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-      />
+      <TransformWrapper>
+        <TransformComponent>
+          <canvas
+            ref={canvasRef}
+            style={{ width: '100%' }}
+          />
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 };
@@ -125,21 +68,7 @@ const redrawWall = (
   canvasWidth: number,
   canvasHeight: number,
   cubeSize: number,
-  scaleFactor: number,
-  panX: number,
-  panY:number,
 ) => {
-  console.log(canvasWidth * scaleFactor, canvasWidth / 2);
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  ctx.save();
-  ctx.transform(
-    scaleFactor,
-    0,
-    0,
-    scaleFactor,
-    panX + (canvasWidth - canvasWidth * scaleFactor) / 2,
-    panY + (canvasHeight - canvasHeight * scaleFactor) / 2,
-  );
   let color = CUBE_DARK_COLOR;
   for (let i = 0; i < CUBES_TOTAL_COLUMNS; i++) {
     for (let j = 0; j < CUBES_TOTAL_ROWS; j++) {
