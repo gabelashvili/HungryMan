3ac996f984b61b6766537632b524cb95c7241b61
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-06-15 17:18:47 +0400

    fix lint error

--- src/components/Cubes/Images.tsx
+++ src/components/Cubes/Images.tsx
@@ -1,27 +1,7 @@
-import { useRef } from 'react';
-import DrawObject from './DrawObject/DrawObject';
-
-const Images = ({ images, selectedObjectId, setSelectedObjectId }: PropsTypes) => {
-  const rootRef = useRef<SVGGElement>(null);
-
+const Images = () => {
   return (
-    <g ref={rootRef}>
-      {images.map((el) => (
-        <DrawObject
-          image={el}
-          key={el.id}
-          isSelected={selectedObjectId === el.id}
-          setSelectedObjectId={setSelectedObjectId}
-        />
-      ))}
-    </g>
+    <div>Images</div>
   );
 };
 
 export default Images;
-
-interface PropsTypes {
-    images: {id:string, file?:File, base64?:string, value?:string }[],
-    selectedObjectId: string,
-    setSelectedObjectId: (val:string) => void
-}
