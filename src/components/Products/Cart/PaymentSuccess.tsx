6135f491f00746f57dde12ad5a383a3743312d7f
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-06-19 15:31:25 +0400

    add styles

--- src/components/Products/Cart/PaymentSuccess.tsx
+++ src/components/Products/Cart/PaymentSuccess.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import './sucess.scss';
 
 const PaymentSuccess = () => {
   return (
-    <div>PaymentSuccess</div>
+    <div className="wrapper">
+      <div className="panel without-header product-order">
+        <div className="panel--content">
+
+          <p className="product-order--description">
+            შეკვეთის დეტალური ინფორმაციის სანახავად გთხოვთ ეწვიოთ შეკვეთების
+            ისტორიას ან დაუკავშირდეთ ჩვენ საპორტს
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
   );
 };
 
