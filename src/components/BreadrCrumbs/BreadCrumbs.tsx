3ac14151a49fd9f851fef4c8fa78fe147843bbb4
gabelashvili1999 <gabelashvili1999@gmail.com>
2022-06-19 14:11:34 +0400

    fix breadcrumb

--- src/components/BreadrCrumbs/BreadCrumbs.tsx
+++ src/components/BreadrCrumbs/BreadCrumbs.tsx
@@ -1,22 +1,37 @@
+import { useLocation } from 'react-router-dom';
 import ArrowIcon from '../../Icons/ArrowIcon';
 import './breadcrumbs.scss';
 
 const BreadCrumbs = () => {
+  const { pathname } = useLocation();
+  const generatePathLabel = () => {
+    if (pathname.includes('products')) {
+      return 'პროდუქტები';
+    }
+    if (pathname.includes('cubes')) {
+      return 'კედელი';
+    }
+    return false;
+  };
   return (
-    <div className="wrapper">
-      <nav className="breadcrumbs">
-        <ul className="breadcrumbs--list">
-          <li className="breadcrumbs--item">
-            <a href="" className="breadcrumbs--link"> მთავარი გვერდი </a>
-            <ArrowIcon />
-          </li>
-          <li className="breadcrumbs--item is-active">
-            <a href="" className="breadcrumbs--link"> მთავარი გვერდი </a>
-            <ArrowIcon />
-          </li>
-        </ul>
-      </nav>
-    </div>
+    generatePathLabel() ? (
+      <div className="wrapper">
+        <nav className="breadcrumbs">
+          <ul className="breadcrumbs--list">
+            <li className="breadcrumbs--item">
+              <a href="#" className="breadcrumbs--link"> მთავარი გვერდი </a>
+              <ArrowIcon />
+            </li>
+            <li className="breadcrumbs--item is-active">
+              <a href="#" className="breadcrumbs--link">
+                {generatePathLabel()}
+              </a>
+              <ArrowIcon />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    ) : null
   );
 };
 
